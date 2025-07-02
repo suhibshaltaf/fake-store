74858308c63dc25747b368f6f6726d046699884d
suhibshaltaf <sohaibshaltafaltrefe1@gmail.com>
2025-07-02 16:51:32 +0300

    upd

--- src/assets/Footer/Footer.jsx
+++ src/assets/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="bg-dark text-light py-4 mt-auto">
       <div className="container text-center">
-        <p className="mb-1">&copy; {new Date().getFullYear()} Fake Store. All rights reserved.</p>
+        <p className="mb-1">&copy;  Fake Store. All rights reserved.</p>
         <small>
           Designed & Developed by <strong>Suhib Shaltaf</strong> — Powered by React & FakeStoreAPI
         </small>
